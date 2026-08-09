77bffb9e6ff5d298435c868c932ddfb2c182a339
Maris Vasile <marisvasile@gmail.com>
2026-08-09 17:09:45 +0300

    fix(email): accept valid contact UUIDs

--- lib/frizeo-email/contactConsent.ts
+++ lib/frizeo-email/contactConsent.ts
@@ -5,7 +5,7 @@ import { supabaseAdmin } from "@/lib/supabase/admin";
 export const MAX_CONSENT_BULK_CONTACTS = 200;
 
 export const CONTACT_UUID_PATTERN =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i;
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 export type ConsentChangeResult =
   | "changed"
